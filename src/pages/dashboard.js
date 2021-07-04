b6193099d80fa82582c12077c5d43fef8c568515
rizkysyukur10 <rizkysyukur10@gmail.com>
2021-07-04 19:06:58 +0700

    Create cards

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,86 @@
+import { Row, Col, Card } from "antd";
+
 const Dashboard = () => {
 	return (
-		<h1>Hello world</h1>
-	)	
+		<div className="content">
+			<Row className='row'>
+				<Col span={12}><h1 className="header-title">Dashboard</h1></Col>
+				<Col span={12}>
+				<div className="bar">
+						<label className="bar-title">Input Calender</label>
+					</div>
+				</Col>
+			</Row>
+			<Row className="row">
+				<Col span={24}>
+					<div className="bar">
+						<label className="bar-title">MARKET INSIGHTS</label>
+					</div>
+				</Col>
+			</Row>
+			<Row className="row">
+				<Col span={4}>
+					<Card>Disini diisi harga dan keranjang</Card>
+				</Col>
+			</Row>
+			<Row className="row" gutter={16}>
+				<Col span={12} className="gutter-row">
+					<Card>
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						Disini diisi dengan chart <br />
+						</Card>
+				</Col>
+				<Col span={6} className="gutter-row">
+					<Card>
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						Disini diisi best selling <br />
+						</Card>
+				</Col>
+				<Col span={6} className="gutter-row">
+					<Card>
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						Disini diisi top competitor <br />
+						</Card>
+				</Col>
+			</Row>
+		</div>
+	)
 }
 
 export default Dashboard;
